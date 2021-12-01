ad3ab2316d67a7b1c6e818a4760a90a6d87379e2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-12-01 13:50:28 +0100

    format & make order of arguments more logical

--- day01/solution.fs
+++ day01/solution.fs
@@ -1,53 +1,51 @@
 s" ../lib.fs" included
 
 : count-increasers { base length }
-  0
-  base @
-  0
-  begin
-   1+
-  dup length < while
-    dup cells base + @ ( count prev i cur )
-    rot ( count i cur prev )
-    dup' ( count i cur cur prev )
-    > if
-      ( count i cur )
-      rot 1+ -rot
-    then
-    swap
-  repeat
-  2drop ;
+    0
+    base @
+    0
+    begin
+        1+
+        dup length < while
+            dup cells base + @ ( count prev i cur )
+            rot ( count i cur prev )
+            dup' ( count i cur cur prev )
+            > if
+                ( count i cur )
+                rot 1+ -rot
+            then
+            swap
+    repeat
+    2drop ;
 : count-sliding-increasers { base length }
-  0
-  base @ base cell+ @ + base cell+ cell+ @ +
-  0
-  begin
-  dup length 3 - < while
-    ( count prev i )
-    2dup dup cells base + @ swap ( count prev i prev arr[i] i )
-    3 + cells base + @ ( count prev i prev arr[i] arr[i+3] )
-    rot + swap - ( count prev i cur )
-    rot dup' ( count i cur cur prev )
-    > if
-      ( count i cur )
-      rot 1+ -rot
-    then
-    swap
-    1+
-  repeat
-  2drop ;
+    0
+    base @ base cell+ @ + base cell+ cell+ @ +
+    0
+    begin
+        dup length 3 - < while
+            ( count prev i )
+            2dup dup cells base + @ swap ( count prev i prev arr[i] i )
+            3 + cells base + @ ( count prev i prev arr[i] arr[i+3] )
+            rot + swap - ( count prev i cur )
+            rot dup' ( count i cur cur prev )
+            > if
+                ( count i cur )
+                rot 1+ -rot
+            then
+            swap
+            1+
+    repeat
+    2drop ;
 
 : main
-  1 arg read-file-into-numbers
-  2dup
+    next-arg s>number? invert throw d>s
+    next-arg read-file-into-numbers
 
-  2 arg s>number? invert throw d>s
-  
-  1 = if
-    count-increasers
-  else
-    count-sliding-increasers
-  then
-  . CR ;
+    rot 1 = if
+        count-increasers
+    else
+        count-sliding-increasers
+    then
+    . CR ;
 
 main bye
--- lib.fs
+++ lib.fs
@@ -3,15 +3,15 @@
 \ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 : 3dup { a b c -- a b c a b c }
-  a b c a b c ;
+    a b c a b c ;
 : 3swap { a b c d e f -- d e f a b c }
-  d e f a b c ;
+    d e f a b c ;
 : 3drop { d e f -- }
-  ;
+;
 : dup' { a b -- a a b }
-  a a b ;
+    a a b ;
 : swap' { a b c -- b a c }
-  b a c ;
+    b a c ;
 
 \ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 \ \\\ File reading & parsing
@@ -20,24 +20,24 @@
 4096 Constant max-line
 : fopen r/o open-file throw ;
 : read-file-into-numbers' { fd-in -- ... n }
-  0
-  max-line cells allocate throw
-  begin
-    ( buffer ) dup
-    ( buffer ) max-line fd-in read-line throw
-  while
-    ( buffer read-length ) dup' s>number? invert throw d>s ( buffer number )
-    ( nread buffer number ) rot 1+ rot ( number nread+1 buffer )
-  repeat
-  ( read-length ) drop
-  fd-in close-file throw
-  ( buffer ) free throw ;
+    0
+    max-line cells allocate throw
+    begin
+        ( buffer ) dup
+        ( buffer ) max-line fd-in read-line throw
+    while
+            ( buffer read-length ) dup' s>number? invert throw d>s ( buffer number )
+            ( nread buffer number ) rot 1+ rot ( number nread+1 buffer )
+    repeat
+    ( read-length ) drop
+    fd-in close-file throw
+    ( buffer ) free throw ;
 : to-array { length -- a-addr length }
-  length cells allocate throw
-  length 0 u+do
-    ( base-addr ) dup length cells + i cells - 1 cells - ( base-addr cur-addr )
-    ( number base-addr cur-addr ) swap' !
-  loop
-  length ;
+    length cells allocate throw
+    length 0 u+do
+        ( base-addr ) dup length cells + i cells - 1 cells - ( base-addr cur-addr )
+        ( number base-addr cur-addr ) swap' !
+    loop
+    length ;
 : read-file-into-numbers ( s u -- a-addr length )
-  fopen read-file-into-numbers' to-array ;
+    fopen read-file-into-numbers' to-array ;
