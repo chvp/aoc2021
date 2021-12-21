546d8f693d6fbca0fb9e27460f43cf656bd490d7
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-12-21 08:54:20 +0100

    No need to keep current player

--- day21/solution.fs
+++ day21/solution.fs
@@ -42,56 +42,49 @@ needs ../lib.fs
 ;
 
 : get-p1-addr
-  { mem p1 p2 s1 s2 p }
+  { mem p1 p2 s1 s2 }
   p1 1 - 10 *
   p2 1 - + 21 *
   s1 + 21 *
   s2 + 2 *
-  p 1 - + 2 *
   cells mem +
 ;
 
 : get-p2-addr
-  ( mem p1 p2 s1 s2 p )
+  ( mem p1 p2 s1 s2 )
   get-p1-addr cell+
 ;
 
 : get-wins-at
-  { mem p1 s1 p2 s2 p }
-  s1 21 >= if
-    1 0 exit
-  then
+  { mem p1 s1 p2 s2 }
   s2 21 >= if
     0 1 exit
   then
-  mem p1 p2 s1 s2 p get-p1-addr @ -1 = if
+  mem p1 p2 s1 s2 get-p1-addr @ -1 = if
     0 0
     4 1 do
       4 1 do
         4 1 do
-          p 1 = if
-            mem p1 i j k + + + 10 mod1 s1 p1 i j k + + + 10 mod1 + p2 s2 2 recurse
-          else
-            mem p1 s1 p2 i j k + + + 10 mod1 s2 p2 i j k + + + 10 mod1 + 1 recurse
-          then
+          mem p2 s2 p1 i j k + + + 10 mod1 s1 p1 i j k + + + 10 mod1 + recurse
           rot + -rot + swap
         loop
       loop
     loop
-    mem p1 p2 s1 s2 p get-p2-addr !
-    mem p1 p2 s1 s2 p get-p1-addr !
+    swap
+    mem p1 p2 s1 s2 get-p2-addr !
+    mem p1 p2 s1 s2 get-p1-addr !
   then
-  mem p1 p2 s1 s2 p get-p1-addr @
-  mem p1 p2 s1 s2 p get-p2-addr @
+  mem p1 p2 s1 s2 get-p1-addr @
+  mem p1 p2 s1 s2 get-p2-addr @
 ;
 
 : simulate-universes
   { p1 p2 }
-  10 10 * 21 * 21 * 2 * 2 * cells allocate throw
-  10 10 * 21 * 21 * 2 * 2 * 0 do
+  10 10 * 21 * 21 * 2 * cells allocate throw
+  10 10 * 21 * 21 * 2 * 0 do
     -1 over i cells + !
   loop
-  p1 0 p2 0 1 get-wins-at
+  p1 0 p2 0 get-wins-at
   max
 ;
 
